a8c9e4b5175c1cf014f8dee5ffec6286e41142bb
mansi-mini <mansi.mini@gmail.com>
2018-03-09 23:10:22 -0500

    assignment checkin

--- features/scenario.feature
+++ features/scenario.feature
@@ -3,12 +3,17 @@ Feature: Simple maths
   As a developer
   I want to increment variables
 
-  Scenario: easy maths
-    Given I go to duck duck go
-    When  I search for WebdriverIO
-    Then  I should see the search results
+    Scenario: easy maths on Figure1signup
+    Given I go to Figure1Signup
+    When I enter SignupDetails
+    Then I should see the ConfirmAccountPage
 
-Scenario: easy maths on google
-    Given I go to google
-    When  I search for WebdriverIO on google
-    Then  I should see the search results on google
+    Scenario: easy maths on Figure1
+    Given I go to Figure1
+    When I enter username
+    Then I should see the error
+
+    Scenario: easy maths on Figure1Passwords
+    Given I go to Figure1Passwords
+    When I enter invalidcharacters
+    Then I should see the characters error
--- features/support/steps.js
+++ features/support/steps.js
@@ -3,59 +3,122 @@ const chai = require('chai')
 const should = chai.should()
 
 "use strict";
+   
+  Given('I go to Figure1Signup', function(callback) {
+    this.browser
+      .init()
+      .url('https://app.figure1.com/account/register').then(function() {
+        callback();
+      })
+  })
+  
+  When('I enter SignupDetails', function(callback) {
+    this.browser
+      .waitForVisible('.register-page__username-input')
+      .setValue('.register-page__username-input','mansi_sqat_20')
+      .setValue('.register-page__email-input','figure1.xyz20@gmail.com')
+      .setValue('.register-page__password-input','Sqat06')
+      .setValue('.register-page__confirm-password-input','Sqat06')
+      .addValue('.register-page__specialties-list',' Student')
+      .waitForVisible('.register-page__specialties-other-list')
+      .addValue('.register-page__specialties-other-list','Nursing Student')
+      .click('.box')
+      .click('.register-page__submit-button')
+      .then(function(){
+      callback();
+      }).catch(function(error){
+        callback(error);
+      })
+     
+     })
+    
+     Then('I should see the ConfirmAccountPage', function(callback) {
+      this.browser.waitForVisible('.email-confirmation-intro',3000)    
+      .$('#ember556').getText('div').getText('h2').then(function(result){
+        result.should.equal("Confirm your account, mansi_sqat_20");
+        callback();
+      }).catch(function(error){
+      callback(error);
+    })
+  
+  }) 
 
 
-Given('I go to duck duck go', function(callback) {
+ Given('I go to Figure1', function(callback) {
   this.browser
     .init()
-    .url('https://duckduckgo.com/').then(function() {
+    .url('https://app.figure1.com/account/register').then(function() {
       callback();
     })
 })
 
-When('I search for WebdriverIO', function(callback) {
+When('I enter username', function(callback) {
   this.browser
-    .setValue('#search_form_input_homepage', 'WebdriverIO')
-    .click('#search_button_homepage').then(function(){
-      callback();
+    .waitForVisible('.register-page__username-input')
+    .setValue('.register-page__username-input','mansi_sqat')
+    .setValue('.register-page__email-input','mansi.mini@gmail.com')
+    .setValue('.register-page__password-input','Sqat06')
+    .setValue('.register-page__confirm-password-input','Sqat06')
+    .addValue('.register-page__specialties-list',' Student')
+    .waitForVisible('.register-page__specialties-other-list')
+    .addValue('.register-page__specialties-other-list','Nursing Student')
+    .click('.box')
+    .click('.register-page__submit-button')
+    .then(function(){
+    callback();
+    }).catch(function(error){
+      callback(error);
     })
-})
+   
+   })
+  
+   Then('I should see the error', function(callback) {
+    this.browser    
+    .waitForVisible('.error-message-light',2000)
+      .getText('.error-message-light').then(function(result){
+          result.should.equal('An account with that username already exists.');
+          callback();
+    }).catch(function(error){
+      callback(error);
+    })
+   
+   })
 
-Then('I should see the search results', function(callback) {
-  this.browser
-    .getTitle().then(function(result){
-        result.should.equal("WebdriverIO at DuckDuckGo");
+   Given('I go to Figure1Passwords', function(callback) {
+    this.browser
+      .init()
+      .url('https://app.figure1.com/account/register').then(function() {
         callback();
-  }).catch(function(error){
-    callback(error);
+      })
   })
- 
- })
-
- Given('I go to google', function(callback) {
-  this.browser
-    .init()
-    .url('https://google.ca/').then(function() {
+  
+  When('I enter invalidcharacters', function(callback) {
+    this.browser
+      .waitForVisible('.register-page__username-input')
+      .setValue('.register-page__username-input','mansi_sqat')
+      .setValue('.register-page__email-input','mansi.mini@gmail.com')
+      .setValue('.register-page__password-input','    ')
+      .setValue('.register-page__confirm-password-input','Sqat06')
+      .addValue('.register-page__specialties-list',' Student')
+      .waitForVisible('.register-page__specialties-other-list')
+      .addValue('.register-page__specialties-other-list','Nursing Student')
+      .click('.box')
+      .click('.register-page__submit-button')
+      .then(function(){
       callback();
+      }).catch(function(error){
+        callback(error);
+      })
+     
+     })
+    
+     Then('I should see the characters error', function(callback) {
+      this.browser    
+      .$('#ember334').getText('span').then(function(result){
+          result.should.equal('Password must be at least 6 characters long');
+        callback(); })
+      .catch(function(error){
+      callback(error);
     })
-})
-
-When('I search for WebdriverIO on google', function(callback) {
-  this.browser
-    .setValue('#lst-ib', 'WebdriverIO')
-    .keys('Enter').then(function(){
-      callback();
-    })
-})
-
-Then('I should see the search results on google', function(callback) {
-  this.browser
-    .getTitle().then(function(result){
-        result.should.equal("WebdriverIO - Google Search");
-        callback();
-  }).catch(function(error){
-    callback(error);
+  
   })
- 
- })
-
